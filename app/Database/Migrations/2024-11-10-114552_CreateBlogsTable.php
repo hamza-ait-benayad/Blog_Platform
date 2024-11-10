1ea008c03adee0e848f6aed08dcec435103af8ed
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2024-11-10 19:01:11 +0100

    blogs table migration

---
+++ app/Database/Migrations/2024-11-10-114552_CreateBlogsTable.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateBlogsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true
+            ],
+            'title' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'content' => [
+                'type' => 'TEXT',
+            ],
+            'user_id' => [
+                'type'       => 'INT',
+                'unsigned'   => true,
+            ],
+            'category_id' => [
+                'type'       => 'INT',
+                'unsigned'   => true,
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('category_id', 'categories', 'id', 'SET NULL', 'CASCADE');
+        $this->forge->createTable('blogs');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('blogs');
+    }
+}
