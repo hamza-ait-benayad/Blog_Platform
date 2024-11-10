28212b702cefdea34ca80ace135b822aeb66901b
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2024-11-10 19:01:31 +0100

    categories table migration

---
+++ app/Database/Migrations/2024-11-10-114546_CreateCategories.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateCategories extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true); // Primary key definition (NOT NULL by default)
+        $this->forge->createTable('categories');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('categories');
+    }
+}
