5508c1a0e4eb0ee28268e44f3cb9f6fe41ef70d5
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2024-11-18 23:25:07 +0000

    update user model (add functions saveResetToken & verifyResetToken)

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,13 +6,13 @@ use CodeIgniter\Model;
 
 class UserModel extends Model
 {
-    protected $table            = 'users';                 
-    protected $primaryKey       = 'id';                    
-    protected $useAutoIncrement = true;                    
-    protected $returnType       = 'array';                 
-    protected $useSoftDeletes   = false;                   
+    protected $table            = 'users';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
 
-    protected $allowedFields    = ['username', 'email', 'password', 'created_at', 'updated_at'];
+    protected $allowedFields    = ['reset_token', 'token_expiry','username', 'email', 'password', 'created_at', 'updated_at'];
 
     protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
@@ -21,7 +21,7 @@ class UserModel extends Model
 
     protected $validationRules = [
         'username' => 'required|min_length[3]|max_length[20]',
-        'email'    => 'required|valid_email|is_unique[users.email]', 
+        'email'    => 'required|valid_email|is_unique[users.email]',
         'password' => 'required|min_length[6]',
     ];
 
@@ -54,4 +54,20 @@ class UserModel extends Model
     protected $afterFind      = [];
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
+
+    public function saveResetToken($email, $token)
+    {
+        $data = [
+            'reset_token' => $token,
+            'token_expiry' => date('Y-m-d H:i:s', strtotime('+1 hour')),
+        ];
+        return $this->where('email', $email)->set($data)->update();
+        }
+
+    public function verifyResetToken($token)
+    {
+        return $this->where('reset_token', $token)
+            ->where('token_expiry >=', date('Y-m-d H:i:s'))
+            ->first();
+    }
 }
