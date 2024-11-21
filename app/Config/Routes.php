c0f1954d818e23834342520b2c12544e9c2b414b
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2024-11-21 16:54:37 +0100

    add some routes for store and delete comments

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,19 +10,25 @@ $routes->post('auth/register', 'AuthController::register');
 $routes->get('auth/login', 'AuthController::login');
 $routes->post('auth/login', 'AuthController::login');
 $routes->get('auth/logout', 'AuthController::logout');
-// $route->get('auth/forgot-password', 'AuthController::forgotPassword');
-// $route->post('auth/forgot-password', 'AuthController::forgotPassword');
-// $route->get('auth/reset-password', 'AuthController::resetPassword');
-// $route->post('auth/reset-password', 'AuthController::resetPassword');
-  $routes->get('/', 'DashboardController::index', ['filter' => 'auth']);
+$routes->get('auth/forgotPassword', 'AuthController::forgotPassword');
+$routes->post('auth/forgotPassword', 'AuthController::forgotPassword');
+$routes->get('auth/resetPassword', 'AuthController::showResetForm');
+$routes->post('auth/resetPassword', 'AuthController::resetPassword');
+$routes->get('/', 'DashboardController::index', ['filter' => 'auth']);
+$routes->get('/myBlogs', 'DashboardController::myBlogs', ['filter' => 'auth']);
 
-$routes->group('blog', ['filter' => 'auth'], function($routes) {
+$routes->group('blogs', ['filter' => 'auth'], function($routes) {
   $routes->get('create', 'BlogController::create');
   $routes->post('store', 'BlogController::store');
   // $routes->get('/edit/(:num)', 'BlogController::edit/$1');
   // $routes->post('/update/(:num)', 'BlogController::update/$1');
   // $routes->get('/delete/(:num)', 'BlogController::delete/$1');
-  // $routes->get('/(:num)', 'BlogController::show/$1');
+  $routes->get('show/(:num)', 'BlogController::show/$1');
+});
+
+$routes->group('comments', ['filter' => 'auth'], function($routes) {
+  $routes->post('store/(:num)', 'CommentController::store/$1');
+  $routes->get('delete/(:num)', 'CommentController::delete/$1');
 });
 
 
