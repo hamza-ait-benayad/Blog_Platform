0289c8bdfb096c409b5f5cab2dee03834e80d1c5
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2024-11-24 16:48:06 +0100

    add function to dashboard controller for get blogs by user id

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -9,9 +9,17 @@ class DashboardController extends BaseController
     public function index(): string
     {
         $blogModel = new BlogModel();
-        
         $dataBlog['blogs'] = $blogModel->getAllBlogs();
-
         return view('dashboard', $dataBlog);
+    }
+
+    public function myBlogs(): string
+    {
+        $userId = session()->get('user_id');
+
+        $blogModel = new BlogModel();
+        $data['blogs'] = $blogModel->getMyBlogs($userId);
+    
+        return view('dashboard', $data);
     }
 }
