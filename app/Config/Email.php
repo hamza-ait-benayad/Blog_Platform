3ae82f5c8cc43a02188a237d0a5f11e509c05bfa
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2024-12-07 23:50:22 +0100

    email configuration

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -1,13 +1,12 @@
 <?php
-
 namespace Config;
 
 use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail;
+    public string $fromName = 'Blog platform';           
     public string $recipients = '';
 
     /**
@@ -18,7 +17,7 @@ class Email extends BaseConfig
     /**
      * The mail sending protocol: mail, sendmail, smtp
      */
-    public string $protocol = 'mail';
+    public string $protocol = 'smtp';
 
     /**
      * The server path to Sendmail.
@@ -28,27 +27,27 @@ class Email extends BaseConfig
     /**
      * SMTP Server Hostname
      */
-    public string $SMTPHost = '';
+    public string $SMTPHost = 'smtp.gmail.com';
 
     /**
      * SMTP Username
      */
-    public string $SMTPUser = '';
+    public string $SMTPUser;
 
     /**
      * SMTP Password
      */
-    public string $SMTPPass = '';
+    public string $SMTPPass; // Your Gmail password or app-specific password
 
     /**
      * SMTP Port
      */
-    public int $SMTPPort = 25;
+    public int $SMTPPort = 587;
 
     /**
      * SMTP Timeout (in seconds)
      */
-    public int $SMTPTimeout = 5;
+    public int $SMTPTimeout = 10;
 
     /**
      * Enable persistent SMTP connections
@@ -77,7 +76,7 @@ class Email extends BaseConfig
     /**
      * Type of mail, either 'text' or 'html'
      */
-    public string $mailType = 'text';
+    public string $mailType = 'html'; // Set to 'html' if sending HTML emails
 
     /**
      * Character set (utf-8, iso-8859-1, etc.)
@@ -87,7 +86,7 @@ class Email extends BaseConfig
     /**
      * Whether to validate the email address
      */
-    public bool $validate = false;
+    public bool $validate = true;
 
     /**
      * Email Priority. 1 = highest. 5 = lowest. 3 = normal
@@ -118,4 +117,15 @@ class Email extends BaseConfig
      * Enable notify message from server
      */
     public bool $DSN = false;
+
+    /**
+     * Constructor to load environment variables
+     */
+    public function __construct()
+    {
+        // Fetch the environment variables dynamically
+        $this->fromEmail = getenv('email_serve'); 
+        $this->SMTPUser = getenv('email_serve');
+        $this->SMTPPass = getenv('password_serve'); // Your Gmail password or app-specific password
+    }
 }
